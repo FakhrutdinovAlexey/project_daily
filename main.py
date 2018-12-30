613220103f6fda58d8347c5164b287aedca7ab00
Alexey Fakhrutdinov <alexal0288@gmail.com>
2018-12-30 23:34:24 +0300

    datetime to_do_added

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import sys
 import json
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTextBrowser, QWidget, QVBoxLayout
-from PyQt5.QtWidgets import QDateTimeEdit, QListWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QListWidgetItem, QDateTimeEdit, QListWidget
 from PyQt5 import uic
 from PyQt5.QtCore import QDate
+
+
+def sorting(first):
+    self_date = ''
+    for elem in reversed(first):
+        if elem == '\n':
+            break
+        else:
+            self_date = elem + self_date
+    month, day, year = self_date.split('/')
+    time = year[3:]
+    year = year[:3]
+
+    return tuple(map(int, [year, month, day]))
 
 
 class MainWindow(QMainWindow):
@@ -11,12 +24,16 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi('app.ui', self)
         self.init_list()
+        self.listWidget.setSortingEnabled(True)
 
         self.calendarWidget.clicked[QDate].connect(self.show_actual_page)
         self.pushButton_save.clicked.connect(self.save_text)
         self.pushButton_clear.clicked.connect(self.clear_text)
         self.pushButton_add.clicked.connect(self.add_to_do)
         self.pushButton_delete.clicked.connect(self.delete_to_do)
+        self.pushButton_leadtime.clicked.connect(self.sort_by_lead_time)
+        self.pushButton_addtime.clicked.connect(self.sort_by_add_time)
+        self.sorted = False
 
     def save_text(self):
         actual_date = self.calendarWidget.selectedDate().toString()
@@ -34,7 +51,8 @@ class MainWindow(QMainWindow):
             self.textBrowser.setText(date['diary'][actual_date])
 
     def add_to_do(self):
-        text = self.textBrowser_2.toPlainText()
+        date_time = f'\n{self.dateTimeEdit.textFromDateTime(self.dateTimeEdit.dateTime())}'
+        text = self.textBrowser_2.toPlainText() + date_time
         number = self.listWidget.count() + 1
         self.listWidget.addItem(f'{number}) ' + text)
         date['to_do_list'].append(text)
@@ -54,10 +72,32 @@ class MainWindow(QMainWindow):
             self.listWidget.takeItem(0)
         self.init_list()
 
+        if self.sorted:
+            self.sort_by_lead_time()
+
     def init_list(self):
         for elem in date['to_do_list']:
             number = self.listWidget.count() + 1
             self.listWidget.addItem(f'{number}) ' + elem)
+
+    def sort_by_lead_time(self):
+        copy_list = date['to_do_list'][:]
+        copy_list = sorted(copy_list, key=sorting)
+
+        for _ in range(self.listWidget.count()):
+            self.listWidget.takeItem(0)
+
+        for elem in copy_list:
+            number = self.listWidget.count() + 1
+            self.listWidget.addItem(f'{number}) ' + elem)
+        self.sorted = True
+
+    def sort_by_add_time(self):
+        for _ in range(self.listWidget.count()):
+            self.listWidget.takeItem(0)
+
+        self.init_list()
+        self.sorted = False
 
 
 if __name__ == '__main__':
